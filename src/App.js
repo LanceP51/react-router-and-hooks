73817dfdf0ec8c9b3df626618fbd728a69478d4f
LanceP51 <appleguy05@live.com>
2020-04-22 12:52:20 -0400

    quick project illustrating react router hooks

--- src/App.js
+++ src/App.js
@@ -1,26 +1,58 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Fragment } from "react";
+import "./index.css"
 
-function App() {
+import { BrowserRouter as Router, Route, Link, Switch, Redirect, useHistory, useParams, useLocation } from "react-router-dom";
+
+export default function App() {
+  const name = 'John Doe'
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   <Router>
+    <main>
+      <nav>
+        <ul>
+          <li><Link to="/">Home</Link></li>
+          <li><Link to={`/about/${name}`}>About</Link></li>
+          <li><Link to="/contact">Contact</Link></li>
+        </ul>
+      </nav>
+
+    <Switch>
+      <Route path="/" exact component={Home} />
+      <Route path="/about/:name"  component={About} />
+      <Route path="/contact"  component={Contact} />
+      <Route render={() => <h1>404: page not found</h1>} />
+    </Switch>
+
+    </main>
+</Router>
   );
 }
 
-export default App;
+const Home = () => (
+  <Fragment>
+    <h1>Home</h1>
+  </Fragment>
+  );
+
+  const About = () => {
+    const { name } = useParams()
+    return (
+    // props.match.params.name
+    <Fragment>
+      { name !== 'John Doe' ? <Redirect to="/" /> : null }
+      <h1>About {name}</h1>
+      <Route component={Contact} />
+    </Fragment>
+  )
+  };
+
+  const Contact = () => {
+    const { pathname } = useLocation();
+
+    return (
+      <Fragment>
+        <h1>Contact</h1>
+        <p>Current URL: {pathname}</p>
+      </Fragment>
+      )
+      };
